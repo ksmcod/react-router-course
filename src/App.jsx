53117967d11dfa1708636358b810cdb9069bcd4e
Kono Steve <stevekono77@gmail.com>
2023-05-10 03:15:22 +0100

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements,Route, RouterProvider } from "react-router-dom"
-import Home from './pages/Home'
-import About from './pages/About'
+import Home from "./pages/Home"
+import About from "./pages/About"
 import RootLayout from "./layouts/RootLayout"
 
 
@@ -8,10 +8,11 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
+      <Route path="about" element={<About/>} />
     </Route>
   )
 )
+
 
 export default function App() {
   return (
