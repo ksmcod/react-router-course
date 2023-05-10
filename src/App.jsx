2463ddb6f405cfd98b8e38352381718658a80bbd
Kono Steve <stevekono77@gmail.com>
2023-05-10 03:52:54 +0100

    Implemented 'Help' section with nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, createRoutesFromElements,Route, RouterProvider } f
 import Home from "./pages/Home"
 import About from "./pages/About"
 import RootLayout from "./layouts/RootLayout"
+import { useEffect } from "react"
+import HelpLayout from "./layouts/HelpLayout"
+import Faq from "./pages/help/Faq"
+import Contact from "./pages/help/Contact"
 
 
 const router = createBrowserRouter(
@@ -9,12 +13,21 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About/>} />
+      <Route path="help" element={<HelpLayout/>}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>
   )
 )
 
 
 export default function App() {
+
+  useEffect(() => {
+    document.title = "React Router Course"
+  },[])
+
   return (
     <RouterProvider router={router} />
   )
